7a54ae80619d2a9e542a2efb2e39dbe8d2415eea
Raymond Baghumian <swordart2050@gmail.com>
2024-04-22 16:17:38 +0330

    Creating UserStatus and Complete Table User

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,21 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name')->nullable();
+            $table->string('username')->nullable();
+            $table->string('mobile')->nullable()->unique();
+            $table->string('email')->nullable()->unique();
+            $table->string('phone')->nullable();
+            $table->string('image')->nullable();
+            $table->boolean('is_admin')->default(false);
+            $table->string('whatsapp')->nullable();
+            $table->string('telegram')->nullable();
+            $table->string('instagram')->nullable();
+            $table->string('status')->default(\App\Enums\UserStatus::Active->value);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
